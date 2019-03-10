54879d197752d89ec98e16568c0af21d7921d031
nimi142 <rappaport.nimrod@gmail.com>
2019-03-10 14:45:22 +0200

    Adding meltan and fixing an off by one error

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Pokedex1
+**I do not claim any rights to all assers present in this program. Every asset in this program is provided by nintendo.**
+
+**Use at your own Risk!**
+
+
+After completing Pokemon Let's Go: Eevee i wanted to catch them all so I opened my pokedex, chose a pokemon and looked where to find it just to find out that its exculsive to pokemon Pikachu. I made this program so i could easily check if a pokemon i chose is in my game or not.
+
+This program checks if the pokemon you searched for (by name or pokemon number) is in each of the games and tells you with the pictures present. if th letter is white, it is in your game.
+
+This version does not have Meltan.
+
--- main.py
+++ main.py
@@ -40,11 +40,7 @@ aEevee = [True, True, True, True, True, True, True, True, True, True, True, True
           True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True,
           True, False, False, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True,
           True, False, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True,
-<<<<<<< HEAD
           True, True, True, True, True, True, True, True,False
-=======
-          True, True, True, True, True, True, True, True, False
->>>>>>> b393f53138775195d961029c641a1286420a1ad3
 
           ]
 aPikachu = [True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True,
@@ -55,11 +51,7 @@ aPikachu = [True, True, True, True, True, True, True, True, True, True, True, Tr
             False, False, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True,
             True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True,
             False, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True,
-<<<<<<< HEAD
             True, True, True, True, True, True, True, True, True, True, True,False
-=======
-            True, True, True, True, True, True, True, True, True, True, True, False
->>>>>>> b393f53138775195d961029c641a1286420a1ad3
 
             ]
 pPoke = None
@@ -134,11 +126,7 @@ def changePokemon(pokeName, pPoke=None):
         lpic.image = pPoke
     lname.configure(text=capFirstLetter(pokeName))
     ldesc.configure(text=("Pokedex num: " + str(aNames.index(pokeName) + 1)))
-<<<<<<< HEAD
     inEevee = aEevee[aNames.index(pokeName) ]
-=======
-    inEevee = aEevee[aNames.index(pokeName)]
->>>>>>> b393f53138775195d961029c641a1286420a1ad3
     inPikachu = aPikachu[aNames.index(pokeName)]
     if inEevee is True:
         leve.configure(image=pYesEevee)
